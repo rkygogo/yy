53e2c390c6206d4b777045bf53bd8cc9c3719a8f
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-07-08 11:27:27 +0800

    Update hyy.sh

--- hyy.sh
+++ hyy.sh
@@ -2,6 +2,7 @@
 red='\033[0;31m'
 bblue='\033[0;34m'
 plain='\033[0m'
+blue(){ echo -e "\033[36m\033[01m$1\033[0m";}
 red(){ echo -e "\033[31m\033[01m$1\033[0m";}
 green(){ echo -e "\033[32m\033[01m$1\033[0m";}
 yellow(){ echo -e "\033[33m\033[01m$1\033[0m";}
@@ -310,8 +311,8 @@ esac
 }
 
 if [[ -n $(systemctl status hysteria-server 2>/dev/null | grep "active") ]]; then
-chmod +x /root/CFwarp.sh 
-ln -sf /root/CFwarp.sh /usr/bin/hy
+chmod +x /root/hyy.sh 
+ln -sf /root/hyy.sh /usr/bin/hy
 fi
 
 
