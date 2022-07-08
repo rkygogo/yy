82651cf26ec7d7996ca622ef481e63c24fce4071
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-07-08 10:36:46 +0800

    Update hyy.sh

--- hyy.sh
+++ hyy.sh
@@ -96,10 +96,9 @@ iptables -t mangle -F >/dev/null 2>&1
 iptables -F >/dev/null 2>&1
 iptables -X >/dev/null 2>&1
 netfilter-persistent save >/dev/null 2>&1
-if [[ $(type -P apache2) ]]; then
-systemctl stop apache2 >/dev/null 2>&1
-systemctl disable apache2 >/dev/null 2>&1
-lsof -i :80|grep -v "PID"|awk '{print "kill -9",$2}'|sh >/dev/null 2>&1
+if [[ -n $(apachectl -v 2>/dev/null) ]]; then
+systemctl stop httpd.service >/dev/null 2>&1
+systemctl disable httpd.service >/dev/null 2>&1
 fi
 }
 
