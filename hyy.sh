df63bc58f542bc96b69f5f0863b41e95c76629ac
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-07-14 13:51:27 +0800

    Update hyy.sh

--- hyy.sh
+++ hyy.sh
@@ -329,9 +329,9 @@ if [[ -n $(systemctl status hysteria-server 2>/dev/null | grep -w active) ]]; th
 noprotocol=`cat /etc/hysteria/config.json 2>/dev/null | grep protocol | awk '{print $2}' | awk -F '"' '{ print $2}'`
 rpip=`cat /etc/hysteria/config.json 2>/dev/null | grep resolve_preference | awk '{print $2}' | awk -F '"' '{ print $2}'`
 [[ $rpip = 64 ]] && v4v6="IPV6优先：$(curl -s6 ip.gs)" || v4v6="IPV4优先：$(curl -s4 ip.gs)"
-status=$(white "hysteria运行状态：\c";green "运行中";white " hysteria运行协议：\c";green "$noprotocol";white " 当前优先出站IP：  \c";green "$v4v6";white " WARP运行状态：    \c";eval echo \$wgcf)
+status=$(white "hysteria运行状态：\c";green "运行中";white "hysteria运行协议：\c";green "$noprotocol";white "当前优先出站IP：  \c";green "$v4v6";white "WARP运行状态：    \c";eval echo \$wgcf)
 else
-status=$(white "hysteria运行状态：\c";red "未启动";white " WARP运行状态：    \c";eval echo \$wgcf)
+status=$(white "hysteria运行状态：\c";red "未启动";white "WARP运行状态：    \c";eval echo \$wgcf)
 fi
 }
 
@@ -362,8 +362,8 @@ green "  6. 卸载hysteria"
 green "  0. 退出脚本 "
 red "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 white " VPS系统信息如下："
-white " 操作系统:         $(blue "$op")" && white " 内核版本:         $(blue "$version")" && white " CPU架构 :         $(blue "$cpu")" && white " 虚拟化类型:       $(blue "$vi")"
-white " $status"
+white "操作系统:         $(blue "$op")" && white "内核版本:         $(blue "$version")" && white "CPU架构 :         $(blue "$cpu")" && white "虚拟化类型:       $(blue "$vi")"
+white "$status"
 echo
 readp "请输入数字:" Input
 case "$Input" in     
