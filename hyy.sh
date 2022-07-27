be57bdfd13d855cae3f8938da3a88bac9b547352
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-07-27 18:47:04 +0800

    Update hyy.sh

--- hyy.sh
+++ hyy.sh
@@ -399,7 +399,7 @@ ip="[$ip]"
 fi
 certificate=`cat /etc/hysteria/config.json 2>/dev/null | grep cert | awk '{print $2}' | awk -F '"' '{ print $2}'`
 if [[ $certificate = '/etc/hysteria/cert.crt' && -z $(curl -s4m5 ip.gs -k) ]]; then
-oldserver=`cat /root/HY/acl/v2rayn.json 2>/dev/null | grep -w server | awk '{print $2}' | awk -F '"' '{ print $2}' | grep -o '\[.*\]'`
+oldserver=`cat /root/HY/acl/v2rayn.json 2>/dev/null | grep -w server | awk '{print $2}' | awk -F '"' '{ print $2}' | grep -o '\[.*\]' | awk '{print $NF}'`
 else
 oldserver=`cat /root/HY/acl/v2rayn.json 2>/dev/null | grep -w server | awk '{print $2}' | awk -F '"' '{ print $2}'| cut -d ':' -f 1`
 fi
