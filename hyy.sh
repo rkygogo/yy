7b1923160b5aca5ba4ee6650b745579339cee5af
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-07-29 13:05:05 +0800

    Update hyy.sh

--- hyy.sh
+++ hyy.sh
@@ -608,8 +608,10 @@ noprotocol=`cat /etc/hysteria/config.json 2>/dev/null | grep protocol | awk '{pr
 rpip=`cat /etc/hysteria/config.json 2>/dev/null | grep resolve_preference | awk '{print $2}' | awk -F '"' '{ print $2}'`
 [[ $rpip = 64 ]] && v4v6="IPV6优先：$(curl -s6 https://ip.gs -k)" || v4v6="IPV4优先：$(curl -s4 https://ip.gs -k)"
 status=$(white "hysteria状态：\c";green "运行中";white "hysteria协议：\c";green "$noprotocol";white "优先出站IP：  \c";green "$v4v6";white "WARP状态：    \c";eval echo \$wgcf)
+elif [[ -z $(systemctl status hysteria-server 2>/dev/null | grep -w active) && -f '/etc/hysteria/config.json' ]]; then
+status=$(white "hysteria状态：\c";yellow "未启动,可尝试选择4，开启或者重启hysteria";white "WARP状态：    \c";eval echo \$wgcf)
 else
-status=$(white "hysteria状态：\c";red "未启动";white "WARP状态：    \c";eval echo \$wgcf)
+status=$(white "hysteria状态：\c";red "未安装";white "WARP状态：    \c";eval echo \$wgcf)
 fi
 }
 
